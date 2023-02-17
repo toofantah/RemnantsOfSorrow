c1fd39c7e979422714127393e60998247040479e
Toofan Tahmouresi <toofan.tah@gmail.com>
2023-02-17 17:27:38 +0300

    Initializing The Project

---
+++ Source/RemnantsOfSorrow/RemnantsOfSorrowGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RemnantsOfSorrowGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARemnantsOfSorrowGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARemnantsOfSorrowGameMode();
+};
+
+
+
